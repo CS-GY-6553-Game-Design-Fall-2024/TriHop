e3286f1195c25ee9b305147dcfb3d0f95fd4b81b
Ryan Kim <kimryan0416@gmail.com>
2024-09-15 15:54:35 -0400

    prototyping lose and win states

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -15,28 +15,83 @@ public class PlayerInput : MonoBehaviour
     [SerializeField] private float m_maxSize = 1f;
     [SerializeField] private float m_minSize = 0.75f;
     [SerializeField] private AnimationCurve m_sizeCurve;
+    [SerializeField] private float m_loseSpinSpeed = 10f;
+    [SerializeField] private float m_winSpinSpeed = 5f;
 
     // Private variables
     private bool m_jumping = false;
     private float m_totalJumpTime, m_jumpStartTime;
     private Vector3 m_startPos, m_endPos;
 
+    public void ResetPlayer() {
+        // If this is called, then we will have to:
+        //  1. Reset the orientation of the player
+        transform.rotation = Quaternion.identity;
+        //  2. Reset the position of the player
+        transform.position = new Vector3(2f, 0f, -0.9f);
+        //  3. Reset the collider of the player
+        m_collider.enabled = true;
+    }
+
     // Update is called once per frame
-    void Update() {
-        // Check the player's current side, which is determined which side the x-position happens to be
-        PlayerSide currentSide = CheckPlayerSide();
+    private void Update() {
 
-        // Check the player's input, which should tell us what the player's intended lane is
-        PlayerSide intendedLane = CheckPlayerInput();
+        // Depending on what the current game mode state is, we have to adjust he behavior of this player
+        switch(TempleJump.current.gameState) {
+            // The game is currently playing
+            case TempleJump.GameState.Play:
+                // Check the player's current side, which is determined which side the x-position happens to be
+                PlayerSide currentSide = CheckPlayerSide();
+                // Check the player's input, which should tell us what the player's intended lane is
+                PlayerSide intendedLane = CheckPlayerInput();
 
-        // Check if there's a discrepancy between the intended lane and the player's current lane (`m_playerSide`)
-        // If there is a discrepancy, and we're not jumping, then we need to initiate the jump mechanism.
-        if (intendedLane != currentSide && !m_jumping) {
-            InitializeJump(intendedLane);
+                // Check if there's a discrepancy between the intended lane and the player's current lane (`m_playerSide`)
+                // If there is a discrepancy, and we're not jumping, then we need to initiate the jump mechanism.
+                // Then, if we are SUPPOSED to be jumping, we update our position
+                if (intendedLane != currentSide && !m_jumping) InitializeJump(intendedLane);
+                if (m_jumping) JumpMovement();
+
+                // Break out of switch statement
+                break;
+
+            // The lose state
+            case TempleJump.GameState.Lose:
+                // If the player has lost... we'll do a stupid spin that conveys frustration. We'll rotate the player around the z-axis
+                transform.Rotate(Vector3.forward, m_loseSpinSpeed);
+
+                // Break out of the switch statement
+                break;
+
+            // The win state
+            case TempleJump.GameState.Win:
+                // If the player has lost... we'll do a stupid spin that conveys frustration. We'll rotate the player around the z-axis
+                transform.Rotate(Vector3.up, m_winSpinSpeed);
+
+                // Break out of the switch statement
+                break;
+
+            // The menu state
+            default:
+                break;
         }
 
-        // Then, if we are SUPPOSED to be jumping, we update our position
-        if (m_jumping) JumpMovement();
+        // If we're currently in the play state (informed by `TempleJump`, then we perform all the jumps and the like)
+        if (TempleJump.current.gameState == TempleJump.GameState.Play) {
+            // Check the player's current side, which is determined which side the x-position happens to be
+            PlayerSide currentSide = CheckPlayerSide();
+            // Check the player's input, which should tell us what the player's intended lane is
+            PlayerSide intendedLane = CheckPlayerInput();
+
+            // Check if there's a discrepancy between the intended lane and the player's current lane (`m_playerSide`)
+            // If there is a discrepancy, and we're not jumping, then we need to initiate the jump mechanism.
+            if (intendedLane != currentSide && !m_jumping) InitializeJump(intendedLane);
+            // Then, if we are SUPPOSED to be jumping, we update our position
+            if (m_jumping) JumpMovement();
+        }
+        // If we're currently in the menu state, then we're in a situation where we have to 
+        else {
+        
+        }
     }
 
     private PlayerSide CheckPlayerSide() {
@@ -83,9 +138,6 @@ public class PlayerInput : MonoBehaviour
         // We prevent jumping if we're not jumping
         if (!m_jumping) return;
 
-        // Disable the collider on this player object
-        m_collider.enabled = false;
-
         // How much of the fraction of the journey are we at? We clamp between 0 and 1
         float journeyFrac = Mathf.Clamp((Time.time - m_jumpStartTime) / m_totalJumpTime, 0f, 1f);
         
@@ -98,24 +150,24 @@ public class PlayerInput : MonoBehaviour
         
         // If we are sufficiently close to the target position (or if journeyFrac == 1f), then we stop the jump
         if (journeyFrac == 1f) {
-            m_collider.enabled = true;
             m_jumping = false;
         }
     }
 	
-	private void GameOver()
-	{
-		Time.timeScale = 1;
+    // We call this function if 
+	private void GameOver() {
+		//Time.timeScale = 1;
+        m_collider.enabled = false;
 		ObstacleGenerator.ResetObstacles();
-		TempleJump.current.SetMenuState();
+		TempleJump.current.SetLoseState();
 	}
 
-	void OnTriggerEnter(Collider other)
-	{
-		if(other.gameObject.tag == "obstacle")
-		{
-			Invoke("GameOver",0.03f);
-			Time.timeScale = 0.07f;
+	void OnTriggerEnter(Collider other) {
+		// if the other object has a tag "obstacle", then we know that we need to force a game over state
+        if(other.gameObject.tag == "obstacle") {
+			GameOver();
+            //Invoke("GameOver",0.03f);
+			//Time.timeScale = 0.07f;
 		}
 	}
 	
--- Assets/Scripts/TempleJump.cs
+++ Assets/Scripts/TempleJump.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class TempleJump : MonoBehaviour
 {
-    public enum GameState { Menu, Play }
+    public enum GameState { Menu=0, Lose=1, Win=2, Play=3 }
     public static TempleJump current;
     
     [Header("=== Current Game State ===")]
     [SerializeField] private GameState m_gameState = GameState.Menu;
+    public GameState gameState => m_gameState;
 
     [Header("=== UI Elements ===")]
     [SerializeField] private Canvas m_menuCanvas;
     [SerializeField] private Canvas m_playingCanvas;
+    [SerializeField] private Canvas m_loseCanvas;
+    [SerializeField] private Canvas m_winCanvas;
     [SerializeField] private Canvas m_debugCanvas;
 
     [Header("=== Game Mechanic Controllers ===")]
@@ -26,7 +29,9 @@ public class TempleJump : MonoBehaviour
     [SerializeField] private bool m_debugMode = false;
     [SerializeField] private KeyCode m_activateDebugKey = KeyCode.Return;
     [SerializeField] private KeyCode m_toggleMenuStateKey = KeyCode.Alpha1;
-    [SerializeField] private KeyCode m_togglePlayStateKey = KeyCode.Alpha2;
+    [SerializeField] private KeyCode m_toggleLoseStateKey = KeyCode.Alpha2;
+    [SerializeField] private KeyCode m_toggleWinStateKey = KeyCode.Alpha3;
+    [SerializeField] private KeyCode m_togglePlayStateKey = KeyCode.Alpha4;
 
     private void Awake() {
         current = this;
@@ -43,7 +48,7 @@ public class TempleJump : MonoBehaviour
         // One caveat is that if the interaction key is already held down (i.e. they lost during gameplay), then...
         //      ... without a check for that, this will automatically default to being called.
         //      So we need to check if the player is transitioning from play to menu state, and use a boolean check here.
-        if (m_gameState == GameState.Menu && m_canSetPlayMode && Input.GetKey(m_interactionKey)) SetPlayState();
+        if ((int)m_gameState <= 2 && m_canSetPlayMode && Input.GetKey(m_interactionKey)) SetPlayState();
         m_canSetPlayMode = !Input.GetKey(m_interactionKey);
         // Purely for debug purposes
         UpdateDebug();
@@ -53,6 +58,8 @@ public class TempleJump : MonoBehaviour
         if (Input.GetKeyDown(m_activateDebugKey)) ToggleDebugMode();
         if (!m_debugMode) return;
         if (Input.GetKeyDown(m_toggleMenuStateKey)) SetMenuState();
+        if (Input.GetKeyDown(m_toggleLoseStateKey)) SetLoseState();
+        if (Input.GetKeyDown(m_toggleWinStateKey)) SetWinState();
         if (Input.GetKeyDown(m_togglePlayStateKey)) SetPlayState();
     }
 
@@ -70,7 +77,38 @@ public class TempleJump : MonoBehaviour
         // Lastly, hide the playing canvas and show the menu canvas
         m_playingCanvas.gameObject.SetActive(false);
         m_menuCanvas.gameObject.SetActive(true);
-        
+    }
+    public void SetLoseState() {
+        // First, let's set the game state itself to the lose state
+        m_gameState = GameState.Lose;
+
+        // Secondly, deactive the ground spawner. We DON'T disable the player though.
+        m_groundSpawner.gameObject.SetActive(false);
+        m_player.transform.rotation = Quaternion.identity;
+
+        // Thirdly, use the boolean check to prevent the interaction key from auto-triggering the play mode if held down.
+        m_canSetPlayMode = !Input.GetKey(m_interactionKey);
+
+        // Lastly, hide the playing canvas and show the menu + lose canvases
+        m_playingCanvas.gameObject.SetActive(false);
+        m_menuCanvas.gameObject.SetActive(true);
+        m_loseCanvas.gameObject.SetActive(true);
+    }
+    public void SetWinState() {
+        // First, let's set the game state itself to the lose state
+        m_gameState = GameState.Win;
+
+        // Secondly, deactive the ground spawner. We DON'T disable the player though.
+        m_groundSpawner.gameObject.SetActive(false);
+        m_player.transform.rotation = Quaternion.identity;
+
+        // Thirdly, use the boolean check to prevent the interaction key from auto-triggering the play mode if held down.
+        m_canSetPlayMode = !Input.GetKey(m_interactionKey);
+
+        // Lastly, hide the playing canvas and show the menu + win canvases
+        m_playingCanvas.gameObject.SetActive(false);
+        m_menuCanvas.gameObject.SetActive(true);
+        m_winCanvas.gameObject.SetActive(true);
     }
     public void SetPlayState() {
         // Firstly, let's set the game state itself to the playing state
@@ -79,9 +117,12 @@ public class TempleJump : MonoBehaviour
         // Secondly, activate any controllers or gameplay elements involved in the Play mode
         m_groundSpawner.gameObject.SetActive(true);
         m_player.gameObject.SetActive(true);
+        m_player.ResetPlayer();
 
         // Lastly, hide the menu canvas and show the playing canvas
         m_menuCanvas.gameObject.SetActive(false);
+        m_loseCanvas.gameObject.SetActive(false);
+        m_winCanvas.gameObject.SetActive(false);
         m_playingCanvas.gameObject.SetActive(true);
     }
     public void SetDebugMode(bool setTo) {
