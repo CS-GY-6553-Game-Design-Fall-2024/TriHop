1032f7cc0e9c66324b715de07f75aa0444c2df3b
Ashley Liu <toashleyliu@gmail.com>
2024-09-15 18:24:49 -0400

    add player jump and collision sounds

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -6,8 +6,13 @@ using UnityEngine;
 public class PlayerInput : MonoBehaviour
 {
     public static PlayerInput current;
-    public enum PlayerSide { Left=-1, Right=1 }
-    
+    public enum PlayerSide { Left = -1, Right = 1 }
+
+    // Sound variables
+    public AudioSource audioSource;
+    public AudioClip jumpSound;
+    public AudioClip collisionSound;
+
     [Header("=== Player Settings ===")]
     [SerializeField] private Collider m_collider;
     [SerializeField] private Transform m_leftTarget;
@@ -24,11 +29,13 @@ public class PlayerInput : MonoBehaviour
     private float m_totalJumpTime, m_jumpStartTime;
     private Vector3 m_startPos, m_endPos;
 
-    private void Awake() {
+    private void Awake()
+    {
         current = this;
     }
 
-    public void ResetPlayer() {
+    public void ResetPlayer()
+    {
         // If this is called, then we will have to:
         //  1. Reset the orientation of the player
         transform.rotation = Quaternion.identity;
@@ -39,10 +46,12 @@ public class PlayerInput : MonoBehaviour
     }
 
     // Update is called once per frame
-    private void Update() {
+    private void Update()
+    {
 
         // Depending on what the current game mode state is, we have to adjust he behavior of this player
-        switch(TempleJump.current.gameState) {
+        switch (TempleJump.current.gameState)
+        {
             // The game is currently playing
             case TempleJump.GameState.Play:
                 // Check the player's current side, which is determined which side the x-position happens to be
@@ -81,7 +90,8 @@ public class PlayerInput : MonoBehaviour
         }
 
         // If we're currently in the play state (informed by `TempleJump`, then we perform all the jumps and the like)
-        if (TempleJump.current.gameState == TempleJump.GameState.Play) {
+        if (TempleJump.current.gameState == TempleJump.GameState.Play)
+        {
             // Check the player's current side, which is determined which side the x-position happens to be
             PlayerSide currentSide = CheckPlayerSide();
             // Check the player's input, which should tell us what the player's intended lane is
@@ -94,16 +104,19 @@ public class PlayerInput : MonoBehaviour
             if (m_jumping) JumpMovement();
         }
         // If we're currently in the menu state, then we're in a situation where we have to 
-        else {
-        
+        else
+        {
+
         }
     }
 
-    private PlayerSide CheckPlayerSide() {
+    private PlayerSide CheckPlayerSide()
+    {
         return (transform.position.x <= 0f) ? PlayerSide.Left : PlayerSide.Right;
     }
 
-    private PlayerSide CheckPlayerInput() {
+    private PlayerSide CheckPlayerInput()
+    {
         // Check the condition of whatever the jump button we set.
         bool isHeldDown = Input.GetKey(TempleJump.current.interactionKey);
 
@@ -114,18 +127,22 @@ public class PlayerInput : MonoBehaviour
         return intendedLane;
     }
 
-    private void InitializeJump(PlayerSide intendedLane) {
+    private void InitializeJump(PlayerSide intendedLane)
+    {
         // When we "initialize" a jum-p, we need to know the following:
         //  1. Based on the jump speed (`m_jumpSpeed`), how much time is actually required?
         float distance = Vector3.Distance(m_leftTarget.position, m_rightTarget.position);
         m_totalJumpTime = distance / m_jumpSpeed;
 
         //  2. What are the start and end positions of your jump?
-        if (intendedLane == PlayerSide.Right) {
+        if (intendedLane == PlayerSide.Right)
+        {
             // We want to move from left to right
             m_startPos = m_leftTarget.position;
             m_endPos = m_rightTarget.position;
-        } else {
+        }
+        else
+        {
             // We want to move from right to left
             m_startPos = m_rightTarget.position;
             m_endPos = m_leftTarget.position;
@@ -137,43 +154,54 @@ public class PlayerInput : MonoBehaviour
         // We've calculated all that we need: the total amount of time required to jump, when we started the jump, and the start/end positiosn of our jump
         // Now we just need to tell the system that we're jumping.
         m_jumping = true;
+
+        // Play jump sound
+        audioSource.PlayOneShot(jumpSound);
     }
 
-    private void JumpMovement() {
+    private void JumpMovement()
+    {
         // We prevent jumping if we're not jumping
         if (!m_jumping) return;
 
         // How much of the fraction of the journey are we at? We clamp between 0 and 1
         float journeyFrac = Mathf.Clamp((Time.time - m_jumpStartTime) / m_totalJumpTime, 0f, 1f);
-        
+
         // Based on this fraction, we lerp from start to end
         transform.position = Vector3.Lerp(m_startPos, m_endPos, journeyFrac);
 
         // Change the relative lcoal scale of the object based on the journey fraction and animation curve
         float m_localScale = m_minSize + m_sizeCurve.Evaluate(journeyFrac) * (m_maxSize - m_minSize);
         transform.localScale = new Vector3(m_localScale, m_localScale, m_localScale);
-        
+
         // If we are sufficiently close to the target position (or if journeyFrac == 1f), then we stop the jump
-        if (journeyFrac == 1f) {
+        if (journeyFrac == 1f)
+        {
             m_jumping = false;
         }
     }
-	
-    // We call this function if 
-	private void GameOver() {
-		//Time.timeScale = 1;
-        m_collider.enabled = false;
-		ObstacleGenerator.ResetObstacles();
-		TempleJump.current.SetLoseState();
-	}
 
-	void OnTriggerEnter(Collider other) {
-		// if the other object has a tag "obstacle", then we know that we need to force a game over state
-        if(other.gameObject.tag == "obstacle") {
-			GameOver();
+    // We call this function if 
+    private void GameOver()
+    {
+        //Time.timeScale = 1;
+        m_collider.enabled = false;
+        ObstacleGenerator.ResetObstacles();
+        TempleJump.current.SetLoseState();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // if the other object has a tag "obstacle", then we know that we need to force a game over state
+        if (other.gameObject.tag == "obstacle")
+        {
+            // Play collision sound
+            audioSource.PlayOneShot(collisionSound);
+
+            GameOver();
             //Invoke("GameOver",0.03f);
-			//Time.timeScale = 0.07f;
-		}
-	}
-	
+            //Time.timeScale = 0.07f;
+        }
+    }
+
 }
